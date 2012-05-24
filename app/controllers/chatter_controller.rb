ca159f8eb96833303564fb6124a76db4aee6da2f
vikas yaligar <vikasyaligar.it@gmail.com>
2012-05-24 07:02:39 +0530

    rdc

--- app/controllers/chatter_controller.rb
+++ app/controllers/chatter_controller.rb
@@ -17,7 +17,7 @@ before_filter :make_websocket , only: [:paserver]
 soc = []
 
 
-EM::WebSocket.start(host: 'afternoon-autumn-2128.herokuapp.com' , port: 1000) do |ws|
+EM::WebSocket.start(host: '127.0.0.1' , port: 4000) do |ws|
 
   ws.onopen{soc << ws;puts "#{soc.length} clients present";ws.send('Welcome to chat server !!')}
   ws.onmessage { |msg| ; soc.each do |s| 
